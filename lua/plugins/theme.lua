c7beeb1a14ca2cc28f49e5c606b163cea4e485e7
Anders Urrang <anders.urrang@unimicro.no>
2023-04-19 22:31:37 +0200

    wip

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,7 +13,7 @@ return {
                 transparent = false,
                 disable_nvimtree_bg = true,
             })
-            vim.cmd.colorscheme 'vscode'
+            -- vim.cmd.colorscheme 'vscode'
         end
     },
     {
@@ -26,12 +26,41 @@ return {
             -- vim.cmd.colorscheme 'catppuccin'
         end
     },
-    -- {
-    --     'AlexvZyl/nordic.nvim',
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         require 'nordic' .load()
-    --     end
-    -- }
+    {
+        'folke/tokyonight.nvim',
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = {},
+                    variables = {},
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+
+            -- vim.cmd.colorscheme 'tokyonight'
+        end
+    },
+    {
+        -- https://github.com/AlexvZyl/nordic.nvim
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require 'nordic'.setup({
+                italic_comments = false,
+                bold_keywords = false,
+                theme = 'onedark'
+            })
+
+            vim.cmd.colorscheme 'nordic'
+        end
+    }
 }
