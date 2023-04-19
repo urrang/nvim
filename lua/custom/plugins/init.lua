b206602a46173fa640ccc15c2fe3cd0eba097ecc
Anders Urrang <anders.urrang@unimicro.no>
2023-04-19 12:22:27 +0200

    wip

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,8 +15,5 @@ return {
       -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' } },
-
     {'ThePrimeagen/vim-be-good'}
 }
