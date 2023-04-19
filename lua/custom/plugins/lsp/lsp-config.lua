e52a4c77eda55ede22b1de0596b1860de907323a
Anders Urrang <anders.urrang@unimicro.no>
2023-04-19 12:15:19 +0200

    rewrite lsp setup

--- lua/custom/plugins/lsp/lsp-config.lua
+++ lua/custom/plugins/lsp/lsp-config.lua
@@ -3,7 +3,7 @@ local M = {}
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities
+M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 M.mason_packages = {
     "css-lsp",
