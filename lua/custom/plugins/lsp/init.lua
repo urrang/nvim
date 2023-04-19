d63f10f0e364bfebc989fb2d4b15e7fe1c57b10b
Anders Urrang <anders.urrang@unimicro.no>
2023-04-19 12:00:35 +0200

    rewrite lsp setup

---
+++ lua/custom/plugins/lsp/init.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        -- event = "BufReadPre",
+        dependencies = {
+            -- "hrsh7th/cmp-nvim-lsp",
+            { 'williamboman/mason.nvim', config = true },
+            'williamboman/mason-lspconfig.nvim',
+            { 'j-hui/fidget.nvim', opts = {} },
+        },
+        config = function()
+            local cfg = require("custom.plugins.lsp.lsp-config")
+            local mason_lspconfig = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
+
+            cfg.setup()
+
+            mason_lspconfig.setup({
+				ensure_installed = vim.tbl_keys(cfg.servers)
+			})
+
+            mason_lspconfig.setup_handlers {
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = cfg.capabilities,
+                        on_attach = cfg.on_attach,
+                        settings = cfg.servers[server_name],
+                    })
+                end,
+            }
+
+        end
+    }
+}
---
+++ lua/custom/plugins/lsp/lsp-config.lua
@@ -0,0 +1,115 @@
+local M = {}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities
+
+M.mason_packages = {
+    "css-lsp",
+    -- "eslint-lsp",
+    "html-lsp",
+    "json-lsp",
+    "lua-language-server",
+    "markdownlint",
+    -- "prettier",
+    -- "tailwindcss-language-server",
+    "typescript-language-server",
+    -- "gopls",
+    -- "editorconfig-checker"
+}
+
+M.servers = {
+    -- gopls = {},
+    -- rust_analyzer = {},
+    tsserver = {},
+    jsonls = {},
+    cssls = {},
+    html = {},
+    -- tailwindcss = {}
+    -- eslint = {}
+
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+-- M.lsp_servers = {
+--     "tsserver",
+--     "lua_ls",
+--     -- "eslint",
+--     "jsonls",
+--     "cssls",
+--     "html",
+--     -- "tailwindcss",
+--     -- "gopls",
+-- }
+
+M.setup = function()
+    vim.diagnostic.config({
+		virtual_text = false,
+		float = {
+			focusable = false,
+			style = "minimal",
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
+		signs = true,
+		underline = true,
+		update_in_insert = true,
+		severity_sort = false,
+	})
+
+    -- show diagnostics in hover window
+	vim.api.nvim_create_autocmd("CursorHold", {
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
+end
+
+M.on_attach = function(_, bufnr)
+    local nmap = function(keys, func, desc)
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+end
+
+return M
