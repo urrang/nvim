cf26fa6c5110cd2f082e72e4fb22eb4382249fb5
Anders Urrang <anders.urrang@unimicro.no>
2023-07-05 00:19:07 +0200

    wip

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,11 +1,13 @@
 local get_header = function(plugin_text)
-	local datetime = os.date(' %d.%m   %H:%M')
+	-- local datetime = os.date(' %d.%m   %H:%M')
 	local v = vim.version();
-	local version_text = 'Version: ' .. v.major .. '.' .. v.minor .. '.' .. v.patch
+	local version_text = '󰘬 ' .. v.major .. '.' .. v.minor .. '.' .. v.patch
 	-- local bottom_text = datetime .. '\t' .. version_text .. '\t' .. (plugin_text or '')
 	local bottom_text = version_text .. '    ' .. (plugin_text or '')
 
 	return {
+		'												   ',
+		'												   ',
 		'												   ',
 		'███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
 		'████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
@@ -66,9 +68,9 @@ return {
 			dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert <CR>'),
 			dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles <CR>'),
 			dashboard.button('g', ' ' .. ' Find text', ':Telescope live_grep <CR>'),
-			dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
+			-- dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
 			dashboard.button('s', ' ' .. ' Restore Session', [[:lua require("persistence").load() <cr>]]),
-			dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
+			-- dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
 			dashboard.button('q', ' ' .. ' Quit', ':qa<CR>'),
 		}
 
@@ -88,14 +90,15 @@ return {
 				local stats = require('lazy').stats()
 				-- local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
 				local ms = math.floor(stats.startuptime + 0.5)
-				local version = vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
+				-- local version = vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
 				--
 				-- -- local plugins = '⚡Neovim' .. version .. ' loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
-				local footer = '⚡Neovim ' .. version .. ' loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
-				dashboard.section.footer.val = footer
+				-- local footer = '⚡Neovim ' .. version .. ' loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+				-- dashboard.section.footer.val = footer
 
 				-- local plugin_text = stats.count .. ' plugins loaded in ' .. ms .. 'ms'
-				local plugin_text = 'Plugins: ' .. stats.count .. '    Load: ' .. ms .. 'ms'
+				-- local plugin_text = ' ' .. stats.count .. '     ' .. ms .. 'ms'
+				local plugin_text = ' Loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
 				dashboard.section.header.val = get_header(plugin_text)
 				pcall(vim.cmd.AlphaRedraw)
 			end,
