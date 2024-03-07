9bd01113b5cf449d5fabf2c3d4b05faf56bf8b38
Anders Urrang <aurrang@gmail.com>
2024-03-07 13:04:13 +0100

    oil.nvim config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,11 +7,20 @@ return {
 	},
 	opts = {
 		view_options = { show_hidden = true },
+		delete_to_trash = true,
+		-- skip_confirm_for_simple_edits = true,
+		lsp_file_methods = { autosave_changes = true },
+		use_default_keymaps = false,
 		keymaps = {
+			['?'] = 'actions.show_help',
+			['<CR>'] = 'actions.select',
+			['<C-p>'] = 'actions.preview',
+			['_'] = 'actions.open_cwd',
+			['gt'] = 'actions.toggle_trash',
 			['<BS>'] = 'actions.parent',
 			['q'] = 'actions.close',
 			['<C-b>'] = 'actions.close',
-			['<Esc>'] = 'actions.close'
+			['<Esc>'] = 'actions.close',
 		}
 	},
 }
