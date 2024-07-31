24bed6b915f83cf67eb262af36d0d4d700c7f971
Anders Urrang <aurrang@gmail.com>
2024-07-31 08:29:12 +0200

    remove plugins from vscode config

--- lua/vscode/vscode.lua
+++ lua/vscode/vscode.lua
@@ -15,25 +15,25 @@ vim.o.undofile = true
 ---------------------------------------------
 
 -- Init Lazy (plugin manager)
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	})
-end
-
-vim.opt.rtp:prepend(lazypath)
-
--- Load plugins
-local plugins = require('vscode/vscode_plugins')
-require('lazy').setup(plugins, {
-	lockfile = vim.fn.stdpath("config") .. "/lazy-lock-vscode.json"
-})
+-- local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+-- if not vim.loop.fs_stat(lazypath) then
+-- 	vim.fn.system({
+-- 		'git',
+-- 		'clone',
+-- 		'--filter=blob:none',
+-- 		'https://github.com/folke/lazy.nvim.git',
+-- 		'--branch=stable', -- latest stable release
+-- 		lazypath,
+-- 	})
+-- end
+--
+-- vim.opt.rtp:prepend(lazypath)
+--
+-- -- Load plugins
+-- local plugins = require('vscode/vscode_plugins')
+-- require('lazy').setup(plugins, {
+-- 	lockfile = vim.fn.stdpath("config") .. "/lazy-lock-vscode.json"
+-- })
 
 
 ---------------------------------------------
