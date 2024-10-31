c4ac896a28ad1c2dcc3d749d201c7a2ffed2c59e
Anders Urrang <aurrang@gmail.com>
2024-10-31 19:56:15 +0100

    grapple: use both git branch and cwd as scope

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -41,7 +41,39 @@ return {
     cmd = 'Grapple',
     keys = keys,
     opts = {
-        scope = 'git_branch',
+        scopes = {
+            {
+                name = 'cwd_branch',
+                desc = 'Current working directory and git branch',
+                fallback = 'cwd',
+                cache = {
+                    event = { 'BufEnter', 'FocusGained' },
+                    debounce = 1000, -- ms
+                },
+                resolver = function()
+                    local git_files = vim.fs.find('.git', {
+                        upward = true,
+                        stop = vim.loop.os_homedir(),
+                    })
+
+                    if #git_files == 0 then
+                        return
+                    end
+
+                    local root = vim.loop.cwd()
+
+                    local result = vim.fn.system({ 'git', 'symbolic-ref', '--short', 'HEAD' })
+                    local branch = vim.trim(string.gsub(result, '\n', ''))
+
+                    local id = string.format('%s:%s', root, branch)
+                    local path = root
+
+                    return id, path
+                end,
+            },
+        },
+        scope = 'cwd_branch',
+        -- scope = 'git_branch',
         style = 'basename',
         tag_title = function()
             return 'Tags'
