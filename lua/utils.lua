b5c0efa039fa73b22d814afa29e315ead22f220f
Anders Urrang <aurrang@gmail.com>
2025-01-06 10:59:23 +0100

    auto convert function to async when typing await

--- lua/utils.lua
+++ lua/utils.lua
@@ -66,4 +66,42 @@ M.open_in_github = function()
     end
 end
 
+local function find_node_ancestor(types, node)
+    if not node then
+        return nil
+    end
+
+    if vim.tbl_contains(types, node:type()) then
+        return node
+    end
+
+    local parent = node:parent()
+
+    return find_node_ancestor(types, parent)
+end
+
+-- When typing "await" add "async" to the function declaration if it doesn't already exist
+M.auto_await = function()
+    vim.api.nvim_feedkeys('t', 'n', true)
+
+    local text_before_cursor = vim.fn.getline('.'):sub(vim.fn.col('.') - 4, vim.fn.col('.') - 1)
+    if text_before_cursor ~= 'awai' then
+        return
+    end
+
+    local current_node = vim.treesitter.get_node({ ignore_injections = false })
+    local function_node = find_node_ancestor({ 'arrow_function', 'function_declaration', 'function' }, current_node)
+    if not function_node then
+        return
+    end
+
+    local function_text = vim.treesitter.get_node_text(function_node, 0)
+    if vim.startswith(function_text, 'async ') then
+        return
+    end
+
+    local start_row, start_col = function_node:start()
+    vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { 'async ' })
+end
+
 return M
