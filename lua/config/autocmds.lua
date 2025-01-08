ffd4259d16bac134a2417d604af04fc079ba3401
Anders Urrang <aurrang@gmail.com>
2025-01-08 13:51:09 +0100

    resize windows when terminal is resized

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,6 +12,12 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
     command = 'silent! normal! g`"zv',
 })
 
+vim.api.nvim_create_autocmd('VimResized', {
+    desc = 'Resize windows when terminal is resized',
+    group = vim.api.nvim_create_augroup('win_autoresize', { clear = true }),
+    command = 'wincmd =',
+})
+
 local is_angular_project = nil
 vim.api.nvim_create_autocmd('BufRead', {
     desc = 'Start treesitter parser for angular on html files in angular project',
