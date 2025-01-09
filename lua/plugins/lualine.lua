8a877c3c38b9312c76c3b67f14585a332d21da83
Anders Urrang <aurrang@gmail.com>
2025-01-09 12:48:30 +0100

    remove grapple tags from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,45 +7,45 @@ local function macro_recording()
     end
 end
 
-local hl_cache = {}
-local function grapple_tags()
-    local contents = {}
-
-    local current_file_path = vim.fn.expand('%:p')
-    local tags = require('grapple').tags()
-
-    local set_icon_highlight = function(highlight, fg, bg)
-        if hl_cache[highlight] then
-            return
-        end
-
-        vim.api.nvim_set_hl(0, highlight, { fg = fg, bg = bg })
-    end
-
-    for i, tag in ipairs(tags) do
-        local file_name = tag.path == '' and '(empty)' or vim.fn.fnamemodify(tag.path, ':t')
-        local extension = file_name:match('.*%.([^%.]+)$')
-
-        local icon, color = require('nvim-web-devicons').get_icon_color(file_name, extension, { default = true })
-
-        if current_file_path == tag.path then
-            local icon_hl = 'lualine_icon_' .. extension .. '_active'
-            set_icon_highlight(icon_hl, color, '#242637')
-            local ic = string.format('%%#%s#%s', icon_hl, icon)
-
-            contents[i] = string.format(' %%#GrappleActive# %s %s %%#GrappleActive#%s ', i, ic, file_name)
-        else
-            local icon_hl = 'lualine_icon_' .. extension .. '_inactive'
-            set_icon_highlight(icon_hl, color, '#1e202d')
-
-            local ic = string.format('%%#%s#%s', icon_hl, icon)
-
-            contents[i] = string.format(' %%#GrappleInactive# %s %s %%#GrappleInactive#%s ', i, ic, file_name)
-        end
-    end
-
-    return table.concat(contents)
-end
+-- local hl_cache = {}
+-- local function grapple_tags()
+--     local contents = {}
+--
+--     local current_file_path = vim.fn.expand('%:p')
+--     local tags = require('grapple').tags()
+--
+--     local set_icon_highlight = function(highlight, fg, bg)
+--         if hl_cache[highlight] then
+--             return
+--         end
+--
+--         vim.api.nvim_set_hl(0, highlight, { fg = fg, bg = bg })
+--     end
+--
+--     for i, tag in ipairs(tags) do
+--         local file_name = tag.path == '' and '(empty)' or vim.fn.fnamemodify(tag.path, ':t')
+--         local extension = file_name:match('.*%.([^%.]+)$')
+--
+--         local icon, color = require('nvim-web-devicons').get_icon_color(file_name, extension, { default = true })
+--
+--         if current_file_path == tag.path then
+--             local icon_hl = 'lualine_icon_' .. extension .. '_active'
+--             set_icon_highlight(icon_hl, color, '#242637')
+--             local ic = string.format('%%#%s#%s', icon_hl, icon)
+--
+--             contents[i] = string.format(' %%#GrappleActive# %s %s %%#GrappleActive#%s ', i, ic, file_name)
+--         else
+--             local icon_hl = 'lualine_icon_' .. extension .. '_inactive'
+--             set_icon_highlight(icon_hl, color, '#1e202d')
+--
+--             local ic = string.format('%%#%s#%s', icon_hl, icon)
+--
+--             contents[i] = string.format(' %%#GrappleInactive# %s %s %%#GrappleInactive#%s ', i, ic, file_name)
+--         end
+--     end
+--
+--     return table.concat(contents)
+-- end
 
 return {
     'nvim-lualine/lualine.nvim',
@@ -134,41 +134,6 @@ return {
                     {},
                 },
                 lualine_y = {},
-                lualine_z = {},
-            },
-            tabline = {
-                --     lualine_a = {},
-                --     lualine_b = {},
-                --     lualine_c = {
-                --         { 'grapple', separator = '' },
-                --         {
-                --             "filename",
-                --             fmt = filename,
-                --             separator = ''
-                --         },
-                --     },
-                --     -- lualine_c = { { harpoon_files } },
-                --     lualine_x = {},
-                --     lualine_y = {},
-                lualine_c = {
-                    -- { 'grapple', separator = '' },
-                    { grapple_tags },
-                    -- {
-                    --     'filetype',
-                    --     colored = true,
-                    --     icon_only = true,
-                    --     separator = ''
-                    -- },
-                    -- {
-                    --     'filename',
-                    --     symbols = {
-                    --         modified = '',
-                    --         unnamed = '',
-                    --         readonly = '',
-                    --     },
-                    --     separator = { left = '', right = '' },
-                    -- },
-                },
                 lualine_z = {
                     {
                         'tabs',
@@ -179,6 +144,20 @@ return {
                     },
                 },
             },
+            -- tabline = {
+            --     lualine_c = {
+            --         { grapple_tags },
+            --     },
+            --     lualine_z = {
+            --         {
+            --             'tabs',
+            --             cond = function()
+            --                 return #vim.fn.gettabinfo() > 1
+            --             end,
+            --             section_separators = { left = '', right = '' },
+            --         },
+            --     },
+            -- },
         }
 
         return options
