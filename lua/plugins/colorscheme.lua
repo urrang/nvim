79102ad1af5a669eb95c875518be798deebaf3bb
Anders Urrang <aurrang@gmail.com>
2025-05-08 10:51:37 +0200

    asdf

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -37,16 +37,19 @@ local kanappuccin = {
     peach = '#e69f77',
     yellow = '#e0c795',
     teal = '#7AA89F',
-    sky = '#7FB4CA',
+    sky = '#7AA89F',
     sapphire = '#7FB4CA',
     blue = '#7E9CD8',
     lavender = '#acb2e9',
     green = '#8a9a7b',
-    red = '#E46876',
-    -- text = '#c2cae6',
-    text = '#b8b4d0',
+    red = '#df8895',
+    -- text = '#b8b4d0',
+    text = '#c0caf5',
     subtext1 = '#b1b7d3',
     subtext0 = '#9fa5bf',
+    -- text = '#CFCFCF',
+    -- subtext1 = '#b1b7d3',
+    -- subtext0 = '#9fa5bf',
     overlay2 = '#8e93ac',
     overlay1 = '#7c8198',
     overlay0 = '#6a6f85',
@@ -56,38 +59,104 @@ local kanappuccin = {
     base = '#1F1F28',
     mantle = '#181820',
     crust = '#16161D',
+
+    -- base = '#1F1F1F',
+    -- mantle = '#2A2A37',
+    -- crust = '#181820',
 }
 
-local catppuccin_aki = {
+local custom_catppuccin2 = {
     rosewater = '#e5cec9',
     flamingo = '#e2baba',
     pink = '#e6b2d8',
-    mauve = '#ad8dbd',
+    mauve = '#B391D0',
     maroon = '#e09096',
-    peach = '#e09f87',
-    yellow = '#e6c193',
+    peach = '#E09F87',
+    -- orange   = { "#E09F87", 11 },
+    yellow = '#e0c795',
     teal = '#83c8be',
     sky = '#88cad5',
     sapphire = '#75b8d6',
-    blue = '#939dbd',
+    blue = '#88A1D6',
     lavender = '#acb2e9',
-    green = '#a1cd93',
-    red = '#df8895',
-    text = '#d1cec9',
+    green = '#ABCA9E',
+    red = '#d98c98',
+    text = '#C8CCE1',
     subtext1 = '#b1b7d3',
     subtext0 = '#9fa5bf',
     overlay2 = '#8e93ac',
     overlay1 = '#7c8198',
-    overlay0 = '#58596e',
-    surface2 = '#454756',
-    surface1 = '#373846',
-    surface0 = '#2c2d39',
-    base = '#292a36',
-    mantle = '#22232e',
-    crust = '#1a1b24',
+    overlay0 = '#6a6f85',
+    surface2 = '#585c71',
+    surface1 = '#474b5e',
+    surface0 = '#35384a',
+    base = '#292A36',
+    mantle = '#22232E',
+    crust = '#1A1B24',
+}
+
+local custom_catppuccin3 = {
+    rosewater = '#e5cec9',
+    flamingo = '#e2baba',
+    pink = '#e6b2d8',
+    mauve = '#AA749F',
+    maroon = '#e09096',
+    peach = '#E09F87',
+    -- orange   = { "#E09F87", 11 },
+    yellow = '#e0c795',
+    teal = '#88afa2',
+    sky = '#88afa2',
+    sapphire = '#88afa2',
+    blue = '#7E97AB',
+    lavender = '#acb2e9',
+    green = '#A2B5C1',
+    red = '#d98c98',
+    text = '#C8CCE1',
+    subtext1 = '#b1b7d3',
+    subtext0 = '#9fa5bf',
+    overlay2 = '#8e93ac',
+    overlay1 = '#7c8198',
+    overlay0 = '#6a6f85',
+    surface2 = '#585c71',
+    surface1 = '#474b5e',
+    surface0 = '#35384a',
+    base = '#1f1f1f',
+    mantle = '#171717',
+    crust = '#171717',
 }
 
 return {
+    {
+        'vague2k/vague.nvim',
+        config = function()
+            require('vague').setup({
+                -- optional configuration here
+            })
+        end,
+    },
+    {
+        'bettervim/yugen.nvim',
+        config = function() end,
+    },
+    {
+        'kvrohit/rasmus.nvim',
+        priority = 1000,
+        config = function() end,
+    },
+
+    {
+        dir = '~/git/no-clown-fiesta.nvim',
+        config = function()
+            require('no-clown-fiesta').setup({
+                transparent = false,
+                styles = {
+                    lsp = { undercurl = true },
+                },
+            })
+
+            -- vim.cmd.colorscheme('no-clown-fiesta')
+        end,
+    },
     -- {
     --     'aktersnurra/no-clown-fiesta.nvim',
     --     config = function()
@@ -106,12 +175,75 @@ return {
     --         })
     --     end,
     -- },
-    -- {
-    --     'gmr458/cold.nvim',
-    --     config = function()
-    --         require('cold').setup({})
-    --     end,
-    -- },
+    {
+        'gmr458/cold.nvim',
+        config = function()
+            require('cold').setup({})
+        end,
+    },
+    {
+        'Mofiqul/vscode.nvim',
+        config = function()
+            local c = require('vscode.colors').get_colors()
+            -- c.vscLightBlue = '#D4D4D4'
+            require('vscode').setup({
+                italic_comments = false,
+                color_overrides = c,
+                group_overrides = {
+                    ['@constant'] = { fg = c.vscLightBlue },
+                    ['@type.builtin'] = { fg = c.vscBlueGreen },
+                    ['@comment'] = { fg = '#808080' },
+                    -- ['@lsp.typemod.variable.readonly.typescript'] = { fg = c.vscLightBlue },
+                },
+            })
+
+            -- vim.cmd.colorscheme('vscode')
+        end,
+    },
+    {
+        'thesimonho/kanagawa-paper.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {
+            colors = {
+                palette = {
+                    -- oldWhite = '#b7b7c8',
+                    -- fujiWhite = '#b7b7c8',
+                    oldWhite = '#c2c1bc',
+                    fujiWhite = '#c2c1bc',
+                    fujiGray = '#727169',
+
+                    --
+                    sumiInk3 = '#1F1F1F',
+                },
+                theme = {
+                    ink = {},
+                },
+            },
+            overrides = function(colors)
+                local palette = colors.palette
+                return {
+                    -- Normal = { bg = '#1f1f1f' },
+                    Identifier = { fg = palette.oldWhite },
+                    ['@lsp.mod.readonly'] = { fg = palette.oldWhite },
+                    ['@lsp.mod.defaultLibrary'] = { fg = palette.dragonBlue2 },
+                    ['@lsp.type.parameter'] = { fg = palette.oldWhite },
+                    ['@variable.parameter'] = { fg = palette.oldWhite },
+                    ['@variable.builtin'] = { italic = false },
+                    ['@operator'] = { fg = palette.dragonGray2 },
+
+                    ['@tag.html'] = { fg = palette.dragonPink },
+
+                    Type = { fg = palette.dragonYellow },
+
+                    -- ['@function'] = { fg = palette.dragonYellow },
+                    -- Identifier = { fg = '#c0c0ce' },
+                    -- ['@variable'] = { fg = '#c0c0ce' },
+                }
+            end,
+        },
+    },
+
     -- {
     --     'sho-87/kanagawa-paper.nvim',
     --     config = function()
@@ -132,29 +264,29 @@ return {
     --         -- vim.cmd.colorscheme('kanagawa-paper')
     --     end,
     -- },
-    -- {
-    --     'rebelot/kanagawa.nvim',
-    --     config = function()
-    --         require('kanagawa').setup({
-    --             commentStyle = { italic = false },
-    --             keywordStyle = { italic = false },
-    --             overrides = function(colors)
-    --                 local palette = colors.palette
-    --                 return {
-    --                     Constant = { fg = palette.oniViolet2 },
-    --                     Identifier = { fg = palette.oniViolet2 },
-    --                     Operator = { fg = palette.oniViolet2 },
-    --                     String = { fg = palette.dragonGreen2 },
-    --                     ['@variable'] = { fg = palette.oniViolet2 },
-    --                     ['@variable.member'] = { fg = palette.oniViolet2 },
-    --                     ['@type.builtin'] = { fg = palette.dragonYellow },
-    --                     ['@keyword.return'] = { fg = palette.oniViolet },
-    --                 }
-    --             end,
-    --             theme = 'dragon',
-    --         })
-    --     end,
-    -- },
+    {
+        'rebelot/kanagawa.nvim',
+        config = function()
+            require('kanagawa').setup({
+                commentStyle = { italic = false },
+                keywordStyle = { italic = false },
+                overrides = function(colors)
+                    local palette = colors.palette
+                    return {
+                        Constant = { fg = palette.oniViolet2 },
+                        Identifier = { fg = palette.oniViolet2 },
+                        Operator = { fg = palette.oniViolet2 },
+                        String = { fg = palette.dragonGreen2 },
+                        ['@variable'] = { fg = palette.oniViolet2 },
+                        ['@variable.member'] = { fg = palette.oniViolet2 },
+                        ['@type.builtin'] = { fg = palette.dragonYellow },
+                        ['@keyword.return'] = { fg = palette.oniViolet },
+                    }
+                end,
+                theme = 'dragon',
+            })
+        end,
+    },
 
     {
         'catppuccin/nvim',
@@ -201,6 +333,7 @@ return {
 
                         ['@variable.builtin'] = { fg = colors.red },
                         ['@constant.builtin'] = { fg = colors.peach },
+                        ['@constant.falsy'] = { fg = colors.peach },
                         ['@function.builtin'] = { fg = colors.blue },
 
                         ['@number'] = { fg = colors.peach },
@@ -286,11 +419,19 @@ return {
                 end,
                 color_overrides = {
                     macchiato = custom_catppuccin,
-                    mocha = catppuccin_aki,
+                    frappe = custom_catppuccin2,
+                    mocha = kanappuccin,
+                    -- mocha = catppuccin_aki,
                 },
             })
 
             vim.cmd.colorscheme('catppuccin')
         end,
+    },
+    {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {},
     },
 }
