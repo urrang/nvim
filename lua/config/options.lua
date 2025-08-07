ff2db7b322139a9ae1370a53c54edbc9646986cd
Anders Urrang <aurrang@gmail.com>
2025-08-07 09:52:44 +0200

    asdf

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -57,7 +57,29 @@ vim.o.termguicolors = true
 -- vim.opt.smartindent = true
 
 vim.diagnostic.config({
+    severity_sort = true,
     float = { border = OPTS.float_border },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+        },
+    },
+    virtual_text = {
+        source = 'if_many',
+        spacing = 2,
+        format = function(diagnostic)
+            local diagnostic_message = {
+                [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                [vim.diagnostic.severity.WARN] = diagnostic.message,
+                [vim.diagnostic.severity.INFO] = diagnostic.message,
+                [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+        end,
+    },
 })
 
 vim.filetype.add({
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,76 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('au-lsp-attach', { clear = true }),
+    callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        local map = function(mode, keys, func, desc)
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename')
+        map('n', '<c-.>', vim.lsp.buf.code_action, 'Code action')
+        map('n', 'gh', vim.lsp.buf.hover, 'Hover docs')
+        map('i', '<C-k>', vim.lsp.buf.signature_help, 'Signature help')
+
+        map('n', 'gd', function()
+            vim.lsp.buf.definition({
+                on_list = function(result)
+                    if vim.tbl_islist(result.items) and #result.items > 0 then
+                        vim.lsp.util.show_document(result.items[1].user_data, 'utf-8')
+                    end
+                end,
+            })
+        end, 'Go to definition')
+
+        -- Highlight references of word under cursor
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+            local highlight_augroup = vim.api.nvim_create_augroup('au-lsp-highlight', { clear = false })
+
+            vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('au-lsp-detach', { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds({ group = 'au-lsp-highlight', buffer = event2.buf })
+                end,
+            })
+        end
+
+        vim.keymap.set('n', 'ge', function()
+            vim.diagnostic.open_float(nil, {
+                focusable = true,
+                -- close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
+                border = OPTS.float_border,
+                scope = 'cursor',
+            })
+        end)
+
+        -- Show diagnostics under cursor
+        -- vim.api.nvim_create_autocmd('CursorHold', {
+        --     desc = 'Show diagnostics on CursorHold',
+        --     callback = function()
+        --         vim.diagnostic.open_float(nil, {
+        --             focusable = false,
+        --             close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
+        --             border = OPTS.float_border,
+        --             scope = 'cursor',
+        --         })
+        --     end,
+        -- })
+    end,
+})
+
 return {
     {
         'neovim/nvim-lspconfig',
