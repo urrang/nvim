9b2a0c7c66eb514519ae45809353558f1dcc0798
Anders Urrang <aurrang@gmail.com>
2026-04-17 10:31:12 +0200

    cleanup: remove vscode and neovide config

--- init.lua
+++ init.lua
@@ -1,16 +1,9 @@
 OPTS = {
-    float_border = vim.g.neovide and 'none' or 'rounded',
-    winblend = vim.g.neovide and 10 or 0,
+    float_border = 'rounded',
+    winblend = 0,
 }
 
-if vim.g.vscode then
-    require('vscode_config.vscode')
-else
-    require('config.options')
-    require('config.lazy')
-    require('config.keymaps')
-    require('config.autocmds')
-    if vim.g.neovide then
-        require('neovide')
-    end
-end
+require('config.options')
+require('config.lazy')
+require('config.keymaps')
+require('config.autocmds')
